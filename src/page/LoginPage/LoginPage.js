1955e25a5e96e872d7960c1bddf266d57a969b7f
kurotamashi30 <kurotamashi@hotmail.co.kr>
2025-03-24 23:07:33 +0900

    test

--- src/page/LoginPage/LoginPage.js
+++ src/page/LoginPage/LoginPage.js
@@ -1,12 +1,12 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Form, Button, Alert } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-// import { GoogleLogin } from '@react-oauth/google';
-// import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleLogin } from '@react-oauth/google';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import './style/login.style.css';
 import { loginWithEmail, loginWithGoogle } from '../../features/user/userSlice';
-//const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
 const Login = () => {
     const dispatch = useDispatch();
@@ -21,10 +21,10 @@ const Login = () => {
         dispatch(loginWithEmail({ email, password }));
     };
 
-    // const handleGoogleLogin = async (googleData) => {
-    //     //구글 로그인 하기
-    //     dispatch(loginWithGoogle(googleData.credential));
-    // };
+    const handleGoogleLogin = async (googleData) => {
+        //구글 로그인 하기
+        dispatch(loginWithGoogle(googleData.credential));
+    };
 
     useEffect(() => {
         if (user) {
@@ -71,14 +71,22 @@ const Login = () => {
                         <p>- Log in with an external account -</p>
 
                         <div className="display-center">
-                            {/* <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+                            {/*1.구글 로그인 버튼 가져오기 ボタン持ってくるgoogle.login 
+                                2.Oauth로그인을 위해서 google api ログインのためGoogleAPI持ってぐっる사이트에 가입하고 클라이언트 키 시크릿 키
+                                3.로그인 기능 만들기 ログイン機能作る
+                                4.백엔드에서 로그인 バックアンドでログイントークン情報あげる
+                                 a. 이미 로그인을 한적이 있는 유저 =>로그인 시키고 토큰값 주면 ok
+                                 b. 처음 로그인시도 유저인 경우 
+                                 =>유저 정보 새로 생성 => 토큰 값을 넘기면 끝 
+                                */}
+                            <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
                                 <GoogleLogin
                                     onSuccess={handleGoogleLogin}
                                     onError={() => {
                                         console.log('Login Failed');
                                     }}
                                 />
-                            </GoogleOAuthProvider> */}
+                            </GoogleOAuthProvider>
                         </div>
                     </div>
                 </Form>
